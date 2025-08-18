e0c3b843c23db504fe1a9df46fc6c68fd45587ee
magnolia.gcloud <magnolia.gcloud@gmail.com>
2025-08-18 09:34:38 +0000

    chore(magnolia): add tailwind and remove mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import './App.css';
-import Button from '@mui/material/Button';
 
 function App() {
   return (
     <>
-      <Button variant="contained">Hello world</Button>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </>
   );
 }
