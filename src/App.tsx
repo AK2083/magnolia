7867c81feef967cb16db7818890ff67dd36c540b
magnolia.gcloud <magnolia.gcloud@gmail.com>
2025-09-01 04:56:19 +0000

    chore(magnolia): extract main part

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,24 @@ import NavigationBar from './shared/components/NavigationBar';
 
 function App() {
   return (
-    <NavigationBar />
+    <>
+      <NavigationBar />
+
+      <div className="py-10">
+        <header>
+          <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+            <h1 className="text-3xl font-bold tracking-tight text-white">
+              Dashboard
+            </h1>
+          </div>
+        </header>
+        <main>
+          <div className="mx-auto max-w-7xl px-4 py-8 sm:px-6 lg:px-8">
+            {/* Your content */}
+          </div>
+        </main>
+      </div>
+    </>
   );
 }
 
