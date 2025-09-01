af1cc0d830809543c496e62d3bc9d7904513fd0e
magnolia.gcloud <magnolia.gcloud@gmail.com>
2025-09-01 04:56:19 +0000

    chore(magnolia): extract main part

--- src/shared/components/NavigationBar.tsx
+++ src/shared/components/NavigationBar.tsx
@@ -120,21 +120,6 @@ export default function NavigationBar() {
             </div>
           </DisclosurePanel>
         </Disclosure>
-
-        <div className="py-10">
-          <header>
-            <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-              <h1 className="text-3xl font-bold tracking-tight text-white">
-                Dashboard
-              </h1>
-            </div>
-          </header>
-          <main>
-            <div className="mx-auto max-w-7xl px-4 py-8 sm:px-6 lg:px-8">
-              {/* Your content */}
-            </div>
-          </main>
-        </div>
       </div>
     </>
   );
